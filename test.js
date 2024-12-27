31c67dadbe4cbcd557fbee9f16eed637a20d1690
kasimblc <kasim.bolucu@livasoft.com.tr>
2024-12-27 14:08:04 +0300

    test js

---
+++ test.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Örneği</title>
+</head>
+<body>
+    <h1>JavaScript Örneği</h1>
+    <button onclick="showMessage()">Mesajı Göster</button>
+
+    <script>
+        function showMessage() {
+            alert("Merhaba, bu bir JavaScript örneğidir!");
+        }
+    </script>
+</body>
+</html>
