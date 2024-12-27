400fd24096ab53858e6961130c8171a038c28317
kasimblc <kasim.bolucu@livasoft.com.tr>
2024-12-27 14:23:35 +0300

    test js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 // Basit bir JavaScript örneği
 
 // Bir sayıyı iki katına çıkaran fonksiyon
-function double(number) {
-    return number * 2;
+function double (number) {
+  return number * 2
 }
 
 // Konsola bir mesaj yazdırma
-console.log("Merhaba, Dünya!");
+console.log('Merhaba, Dünya!')
 
 // Fonksiyonu test et
-let sayi = 5;
-console.log("Sayının iki katı: " + double(sayi));
+const sayi = 5
+console.log('Sayının iki katı: ' + double(sayi))
