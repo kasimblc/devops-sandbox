90270a70f0d4587478a28d5ecec9ca8d77a217d2
kasimblc <kasim.bolucu@livasoft.com.tr>
2024-12-27 12:38:43 +0300

    test push

--- tst.py
+++ tst.py
@@ -7,3 +7,19 @@ def greet(name=None):
 
 greet("Alice")
 greet()
+
+# Yeni eklenen fonksiyonlar
+def farewell(name=None):
+    if name:
+        print(f"Goodbye, {name}!")
+    else:
+        print("Goodbye, World!")
+
+farewell("Bob")
+farewell()
+
+def add(a, b):
+    return a + b
+
+print(add(3, 5))
+print(add(10, 20))
